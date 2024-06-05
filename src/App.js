5bf5d80a4b62e9fe2a8ca63747dc03bb83f6bb29
Marat <447599489marat@gmail.com>
2024-06-05 20:12:31 +0300

    Simple react app created. Unnecessary removed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+  const [counter, setCount]=useState(0)
+
+  function Increment(){
+    setCount(counter + 1)
+  }
+  function Decrement(){
+    setCount(counter - 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      [<div>
+        App works!
+      </div>,
+        React.createElement("button", {
+          disabled: false,
+          onClick: () => console.log('clicked')
+        }, 'App works!'),
+
+        <div>
+          <h1>{counter}</h1>
+          <button onClick={Increment}>Increment</button>
+          <button onClick={Decrement}>Decrement
+          </button>
+        </div>
+      ]
   );
 }
 
